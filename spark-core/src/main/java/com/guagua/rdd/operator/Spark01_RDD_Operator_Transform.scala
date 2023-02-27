99793dc25d85f31e1ac2934ed6def79994b24470
smile watermelon <18516833610@163.com>
2023-02-27 20:15:29 +0800

    map operator

---
+++ spark-core/src/main/java/com/guagua/rdd/operator/Spark01_RDD_Operator_Transform.scala
@@ -0,0 +1,25 @@
+package com.guagua.rdd.operator
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_RDD_Operator_Transform {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf()
+    sparkConf.setMaster("local[*]").setAppName("RDD")
+    val sc: SparkContext = new SparkContext(sparkConf)
+
+
+    val url: RDD[String] = sc.textFile("data/apache.log").map(line => {
+      val fields: Array[String] = line.split(" ")
+      val length: Int = fields.length
+      fields(length - 1)
+    })
+
+    url.saveAsTextFile("output")
+
+    sc.stop()
+  }
+
+}
